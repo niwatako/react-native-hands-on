d9369388ea988136a73b8174a923455f43826aaa
niwatako <git@niwatako.info>
2017-04-22 10:11:13 +0900

    検索ワードを取得する

--- index.ios.js
+++ index.ios.js
@@ -8,19 +8,30 @@ import React, { Component } from 'react';
 import { AppRegistry, View, TextInput, Button } from 'react-native';
 
 export default class ReactNativeGithubSearch extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
+
+  render () {
     return (
       <View style={{ marginTop: 180, paddingHorizontal: 20 }}>
         <TextInput
           style={{ height: 38, borderWidth: 1, borderColor: '#cccccc' }}
           placeholder="Search Github"
+          onChangeText={(text) => this.setState({text: text})}
           />
         <Button
-          onPress={() => console.log("Button on Press!!")}
+          onPress={() => this.onPressSearch(this.state.text)}
+          disabled={this.state.text.length < 1}
           title="Search"
           />
       </View>
     );
+  }
+
+  onPressSearch(search_words) {
+    console.log(search_words);
   }
 }
 
